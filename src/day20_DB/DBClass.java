b8a90902f57dabb213daa08621dfa7403d804885
eun <eb3020@naver.com>
2021-09-02 19:15:12 +0900

    1번2번기능추가

--- src/day20_DB/DBClass.java
+++ src/day20_DB/DBClass.java
@@ -2,36 +2,85 @@ package day20_DB;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class DBClass {
 	private String url = "jdbc:oracle:thin:@210.221.253.215:1521:xe";
 	private String id = "eun";
 	private String pwd = "asd";
-	
-	
+
 	public DBClass() {
 		// 1. 자바에서 오라클에 관련된 기능을 사용할 수 있게 라이브러리 기능을 등록하는것
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public ArrayList<StudentDTO> getList() {
+		ArrayList<StudentDTO> arr = new ArrayList<StudentDTO>();
+
+		// 2. 데베연결
+		try {
+			Connection con = DriverManager.getConnection(url, id, pwd);
+			System.out.println("연결이 잘 이뤄졌습니다\n");
+
+			// 3. 데베 연결된 객체 이용해서 명령어를 수행할 수 잇는 객체를 얻어온다
+			String sql = "select * from newst";
+			PreparedStatement ps = con.prepareStatement(sql);
+
+			// 4. 명령어를 수행할 수 있는 객체를 이용해서 명령어 수행
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+
+				/*
+				 * System.out.println("id = "+rs.getString("id"));
+				 * System.out.println("name = "+rs.getString("name"));
+				 * System.out.println("age = "+rs.getInt("age"));
+				 * System.out.println("------------------------------");
+				 */
+				StudentDTO dto = new StudentDTO();
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setAge(rs.getInt("age"));
+				arr.add(dto);
+			}
+			// 5. 수행 결과를 저장한다
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return arr;
+	}
+
+	public StudentDTO searchST(String id) {
+		//select * from newst where id = '222';
+		String sql = "select * from newst where id = '" +id+ "'";
+		StudentDTO dto =null;
+		
+		try {//1.디비연결
+			Connection con = DriverManager.getConnection(url,this.id,pwd);
+			System.out.println("연결확인");
+			//2.명령어(전송)객체생성
+			PreparedStatement ps =con.prepareStatement(sql);
+			//3.전송 후 결과값 저장
+			ResultSet rs = ps.executeQuery();
 			
-			Connection con=DriverManager.getConnection(null);//객체
-			
+			if(rs.next()) { //데이터가 있으면 저장하라
+				dto = new StudentDTO();
+				dto.setId(rs.getString("id"));
+				dto.setId(rs.getString("name"));
+				dto.setId(rs.getString("age"));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+		return dto;
 	}
-	public void getList() {
-		// 2. 데베연결
-		try {
-	Connection con = DriverManager.getConnection(url,id,pwd);
-	System.out.println("연결이 잘 이뤄졌습니다");
-		// 3. 데베 연결된 객체 이용해서 명령어를 수행할 수 잇는 객체를 얻어온다
-		// 4. 명령어를 수행할 수 있는 객체를 이용해서 명령어 수행
-		// 5. 수행 결과를 저장한다
-		
-	}catch (Exception e) {
-		e.printStackTrace();
-	}
-}
 }
--- src/day20_DB/MainClass.java
+++ src/day20_DB/MainClass.java
@@ -1,34 +1,53 @@
 package day20_DB;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainClass {
-public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	int num,age;
-	String id, name;
-	
-	DBClass db = new DBClass();
-	
-	while(true) {
-		System.out.println("1.모든 데이터 보기 2.검색 3.저장 4.삭제 5.수정");
-		num = input.nextInt();
-		switch(num) {
-		case 1:
-			db.getList();
-			//데이터베이스의 모든 데이터를 가져와서 보여준다
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num, age;
+		String id, name;
+		ArrayList<StudentDTO> arr = null;
+		DBClass db = new DBClass();
+
+		while (true) {
+			System.out.println("1.모든 데이터 보기 2.검색 3.저장 4.삭제 5.수정");
+			num = input.nextInt();
+			switch (num) {
+			case 1:
+				arr = db.getList();
+				// 데이터베이스의 모든 데이터를 가져와서 보여준다
+				for (int i = 0; i < arr.size(); i++) {
+					System.out.println("id = " + arr.get(i).getId());
+					System.out.println("name = " + arr.get(i).getName());
+					System.out.println("age = " + arr.get(i).getAge());
+					System.out.println("-------------");
+				}
+				break;
+			case 2:
+
+				// 검색 데이터를 데베에서 가져오기ㅇ
+				System.out.print("검색id입력 : ");
+				id = input.next();
 			
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			break;
+				StudentDTO dto = db.searchST(id);
+				if(dto != null) {
+				System.out.println("id = " + dto.getId());
+				System.out.println("name = " + dto.getName());
+				System.out.println("age = " + dto.getAge());
+				}else {
+					System.out.println("해당 아이디는 존재하지 않습니다");
+				}
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			}
 		}
+
 	}
-	
-}
 }
